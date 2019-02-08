12d99b30f28ce5dcb2276bb14fcb418b385e8535
Unknown <janeyslinger@gmail.com>
2019-02-08 09:30:34 +0000

    Find events today
    + printTodayTimetable() - prints the events taking place on the current day
    + extractData() - moved code to process the component into a separate function to reduce code duplication

--- IcalTests/calendarTest.py
+++ IcalTests/calendarTest.py
@@ -4,6 +4,14 @@ from datetime import datetime, date
 # opening and reading the values from the ical file using the icalendar module
 # https://stackoverflow.com/questions/3408097/parsing-files-ics-icalendar-using-python
 
+# data I need to get from the event:
+# Module Name
+# LOCATION - already got
+# time and date - already got
+# lecturer - get from description
+# duration?
+# type
+
 
 def printWholeCalendar():
     '''Prints out the whole calendar with the summary, description, start, and end times'''
@@ -30,17 +38,34 @@ def printSelectModule(module):
             thing_to_decode = component.get('summary')
             if thing_to_decode.find(module) != -1:
                 # if the module name is part of the summary then print the information
+                extractData(component)
+    icalFile.close()
 
-                print(component.get('summary'))
-                print(component.get('description'))
-                print(component.get('location'))
-                # the .dt converts the time to datetime object so that it can be read and processed more easily
-                # example of this taken from https://stackoverflow.com/questions/26238835/parse-dates-with-icalendar-and-compare-to-python-datetime
-                print(component['DTSTART'].dt)
-                print(component['DTEND'].dt)
+
+def printTodayTimetable():
+    '''Print the lectures for the current day'''
+    icalFile = open('calendar.ics', 'rb')
+    calendar_to_parse = Calendar.from_ical(icalFile.read())
+    for component in calendar_to_parse.walk():
+        if component.name == "VEVENT":
+            if component['DTSTART'].dt.date() == datetime.today().date():
+                # if the lecture start date is today then print the information
+                extractData(component)
 
     icalFile.close()
 
 
+def extractData(component):
+    print(component.get('summary'))
+    print(component.get('description'))
+    print(component.get('location'))
+    # the .dt converts the time to datetime object so that it can be read and processed more easily
+    # example of this taken from https://stackoverflow.com/questions/26238835/parse-dates-with-icalendar-and-compare-to-python-datetime
+    print(component['DTSTART'].dt)
+    print(component['DTEND'].dt)
+    print("\n")
+
+
 # printWholeCalendar()
-printSelectModule("Computer Graphics")
+#printSelectModule("Computer Graphics")
+printTodayTimetable()
